e248060176d584cf22afe703cf259354d841aee8
keertanapriya-predigle <keertanapriya.dinesh@predigle.com>
2026-01-05 15:40:13 +0530

    Update urls.py

--- anomaly_backend/anomalies/urls.py
+++ anomaly_backend/anomalies/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from .views import anomaly_stream
 
-# urlpatterns = [
-#     path('anomalies/', get_anomalies, name='get-anomalies'),
-
-# ]
-
-# urls.py
 urlpatterns = [
     path("stream/anomalies/", anomaly_stream),
 ]
