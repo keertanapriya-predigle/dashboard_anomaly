cc36712841b73724174689dfe83e893efb41b6c9
keertanapriya-predigle <keertanapriya.dinesh@predigle.com>
2026-01-05 15:39:32 +0530

    Update views.py

--- anomaly_backend/anomalies/views.py
+++ anomaly_backend/anomalies/views.py
@@ -1,33 +1,4 @@
 # views.py
-# from django.http import StreamingHttpResponse
-# import json, time
-# from .models import Anomaly
-# from django.utils.timezone import localtime
-
-# def anomaly_stream(request):
-#     def event_stream():
-#         while True:
-#             anomalies = list(
-#                 Anomaly.objects
-#                 .order_by('-detected_at')[:20]
-#                 .values(
-#                     "customer_id",
-#                     "product_id",
-#                     "anomaly_type",
-#                     "details",
-#                     "detected_at",
-#                 )
-#             )
-#             for a in anomalies:
-#                 a['detected_at'] = localtime(a['detected_at']).isoformat()
-
-#             yield f"data: {json.dumps(anomalies)}\n\n"
-#             time.sleep(5)
-#     return StreamingHttpResponse(
-#         event_stream(),
-#         content_type='text/event-stream'
-#     )
-
 import json, time
 from django.http import StreamingHttpResponse
 from django.utils.timezone import now
